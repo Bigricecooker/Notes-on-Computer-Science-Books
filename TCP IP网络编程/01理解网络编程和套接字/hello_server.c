eb9334247f72b293ebf8b8b3ebd7a4986adfbad1
Bigricecooker <125994306+Bigricecooker@users.noreply.github.com>
2024-05-29 17:42:48 +0800

    Update hello_server.c

--- TCP IP网络编程/01理解网络编程和套接字/hello_server.c
+++ TCP IP网络编程/01理解网络编程和套接字/hello_server.c
@@ -28,32 +28,32 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 
-	//µ÷ÓÃsocket´´½¨Ì×½Ó×Ö
+	//è°ƒç”¨socketåˆ›å»ºå¥—æ¥å­—
 	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 	if (serv_sock == -1)
 		error_handling("socket() error");
 
-	//³õÊ¼»¯serv_addr
+	//åˆå§‹åŒ–serv_addr
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_addr.sin_port = htonl(atoi(argv[1]));
+	serv_addr.sin_port = htons(atoi(argv[1]));
 
-	//µ÷ÓÃbind·ÖÅäIPµØÖ·ºÍ¶Ë¿ÚºÅ
+	//è°ƒç”¨bindåˆ†é…IPåœ°å€å’Œç«¯å£å·
 	if(bind(serv_sock, (struct sockaddr_in*)&serv_addr,sizeof(serv_addr))==-1)
 		error_handling("bind() error");
 
-	//µ÷ÓÃlisten½«Ì×½Ó×Ö×ªÎª¿É½ÓÊÕÁ¬½Ó×´Ì¬
+	//è°ƒç”¨listenå°†å¥—æ¥å­—è½¬ä¸ºå¯æ¥æ”¶è¿æ¥çŠ¶æ€
 	if(listen(serv_sock,5)==-1)
 		error_handling("listen() error");
 
-	//µ÷ÓÃacceptº¯ÊıÊÜÀíÁ¬½ÓÇëÇó
+	//è°ƒç”¨acceptå‡½æ•°å—ç†è¿æ¥è¯·æ±‚
 	clnt_addr_size = sizeof(clnt_addr);
 	clnt_sock = accept(serv_sock, (struct sockaddr_in*)&clnt_addr, &clnt_addr_size);
 	if (clnt_sock == -1)
 		error_handling("accept() error");
 
-	//´«ÊäÊı¾İ
+	//ä¼ è¾“æ•°æ®
 	write(clnt_sock, message, sizeof(message));
 	close(clnt_sock);
 	close(serv_sock);
